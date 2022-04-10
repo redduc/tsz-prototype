23bbbef7c24dc0c1300c9fa46ef66e65fbda8f3a
redduc <rlanza@clip.ch>
2022-04-10 20:45:03 +0200

    remove test timer

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import {useEffect, useState} from "react";
 export default function Home() {
   const titleWords = ["Spielfilme", "Dokfilme", "Werbung", "Events"]
   const [titleWord, setTitleWord] = useState(titleWords[0])
-  const [timer, setTimer] = useState(0)
 
   useEffect(() => {
     let timerId;
