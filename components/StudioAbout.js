1d7d7ed4556ad0f1ca453699f740280415350d5e
Markus Schneider <markus.schneider@clip.ch>
2023-02-28 10:56:20 +0100

    added studios intro

--- components/StudioAbout.js
+++ components/StudioAbout.js
@@ -5,8 +5,38 @@ import styles from '/components/StudioAbout.module.css'
 
 export default function StudioAbout() {
   return (
-    <>
-      {/* <div className={styles.studios + ' ' + styles.studioa} style="background-image: url('about/studioa.jpg');"> */}
+    <>  
+      <div className={styles.wrapperTwoColumn + ' ' + styles.whitebg}>
+        <div className={styles.imageWrapper}>
+          <img src="./about/studios-intro.jpg" className={styles.imgLeft} />
+        </div>      
+        <div className={styles.textWrapper}>
+          <Row className={styles.menuItems}>
+            <Col xs={3} md={6} lg={3}>
+              Service
+            </Col>
+            <Col xs={3} md={6} lg={3}>
+              About
+            </Col>
+            <Col xs={3} md={6} lg={3}>
+              Project
+            </Col>
+            <Col xs={3} md={6} lg={3}>
+              Contact
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <h1>Studios</h1>
+            </Col>
+            <Col xs={12}>
+              <p>Seit 30 Jahren erarbeiten wir hochwertige Tonprodukte für sämtliche Medien. Damit Sie bei uns die erste
+                Geige spielen können, unterstützen wir Sie bei Ihrem Projekt mit unserer langjährigen Erfahrung, hoher
+                Qualität und einer erstklassigen Dienstleistungspalette.</p>
+            </Col>
+          </Row>
+        </div>
+      </div>
       <div className={styles.studios + ' ' + styles.studioa} style={{backgroundImage: "url('about/studioa.jpg')"}}>
         <div className={styles.text}>
           <h2>STUDIO A - CINEMA</h2>
